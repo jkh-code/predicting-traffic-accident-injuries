fce3eebe3f7c81c185b90d6a6d2e15297a85974d
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2021-08-02 13:02:36 -0700

    Add convert_df_columns() function

--- src/raw_to_transformed_data.py
+++ src/raw_to_transformed_data.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import numpy as np
 
 import psycopg2 as pg2
 from os import environ
 
-from typing import Union
+from typing import Union, Literal, List
 
 pd.set_option("display.max_columns", None)
 
@@ -33,6 +34,24 @@ def get_sql_data(
         
     conn.close()
     return df
+
+def convert_df_columns(
+        conversion_type: Literal["datetime", "float", "integer", "string"], 
+        df: pd.DataFrame, columns: List[str])-> None:
+    if conversion_type not in ("datetime", "float", "integer", "string"):
+        raise ValueError(
+            "'converstion_type' must be a value of 'datetime', "
+            + "'float', 'integer', or 'string'.")
+    
+    for col in columns:
+        if conversion_type == "datetime":
+            df[col] = pd.to_datetime(df[col], format="%Y-%m-%dT%H:%M:%S.%f")
+        elif conversion_type == "float":
+            df[col] = pd.to_numeric(df[col], downcast="float")
+        elif conversion_type == "integer":
+            df[col] = pd.to_numeric(df[col], downcast="integer")
+        elif conversion_type == "string":
+            df[col] = df_crashes[col].astype("string")
 
 
 if __name__ == '__main__':
