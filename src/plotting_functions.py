f6d20e2b00bd95bcd4f821c71a2942c1e2e0bf69
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2021-08-03 16:57:05 -0700

    Add percent plot option

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -11,15 +11,28 @@ def injury_vs_no_injury_plot(
         figsize: Tuple[int, int]=(10, 7),
         title: str="TBD",
         xlabel: str="TBD",
-        ylabel: str="TBD"
+        ylabel: str="TBD",
+        percents: bool=False
         ) -> Tuple[Figure, Axes]:
+    """Plot grouped bar chart with one group as 'No' injury crashes and the
+    other group as 'Yes' injury crashes."""
     fig, ax = plt.subplots(figsize=figsize)
-    (df.groupby(["has_injuries", feature_to_plot])["crash_record_id"]
-        .count()
-        .reset_index()
-        .set_index("has_injuries")
-        .pivot(columns=feature_to_plot, values="crash_record_id")
-        .fillna(0.0)).plot(kind="bar", ax=ax)
+    if percents:
+        perc = (
+            (df.groupby(["has_injuries", feature_to_plot])["crash_record_id"]
+                .count()) 
+            / (df.groupby(["has_injuries"])["crash_record_id"].count()))
+        (perc.reset_index()
+            .set_index("has_injuries")
+            .pivot(columns=feature_to_plot, values="crash_record_id")
+            .fillna(0.0)).plot(kind="bar", ax=ax)
+    else:
+        (df.groupby(["has_injuries", feature_to_plot])["crash_record_id"]
+            .count()
+            .reset_index()
+            .set_index("has_injuries")
+            .pivot(columns=feature_to_plot, values="crash_record_id")
+            .fillna(0.0)).plot(kind="bar", ax=ax)
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
