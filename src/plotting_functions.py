3fdf16a393f1666d6f1eaea0495ac4f52b12f4e6
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2021-08-03 17:52:39 -0700

    Add base feature to injury versus no injury function

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -8,6 +8,7 @@ from typing import Tuple
 def injury_vs_no_injury_plot(
         df: pd.DataFrame,
         feature_to_plot: str,
+        base_feature: str="has_injuries",
         figsize: Tuple[int, int]=(10, 7),
         title: str="TBD",
         xlabel: str="TBD",
@@ -19,25 +20,28 @@ def injury_vs_no_injury_plot(
     fig, ax = plt.subplots(figsize=figsize)
     if percents:
         perc = (
-            (df.groupby(["has_injuries", feature_to_plot])["crash_record_id"]
+            (df.groupby([base_feature, feature_to_plot])["crash_record_id"]
                 .count()) 
-            / (df.groupby(["has_injuries"])["crash_record_id"].count()))
+            / (df.groupby([base_feature])["crash_record_id"].count()))
         (perc.reset_index()
-            .set_index("has_injuries")
+            .set_index(base_feature)
             .pivot(columns=feature_to_plot, values="crash_record_id")
             .fillna(0.0)).plot(kind="bar", ax=ax)
     else:
-        (df.groupby(["has_injuries", feature_to_plot])["crash_record_id"]
+        (df.groupby([base_feature, feature_to_plot])["crash_record_id"]
             .count()
             .reset_index()
-            .set_index("has_injuries")
+            .set_index(base_feature)
             .pivot(columns=feature_to_plot, values="crash_record_id")
             .fillna(0.0)).plot(kind="bar", ax=ax)
     ax.set_title(title)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.set_xticklabels(["No", "Yes"])
-    ax.legend(title=None)
+    if base_feature == "has_injuries":
+        ax.set_xticklabels(["No", "Yes"])
+        ax.legend(title=None)
+    elif base_feature == "injury_category":
+        ax.legend(title=None, loc="upper left", bbox_to_anchor=(1, 1))
     return fig, ax
 
 
