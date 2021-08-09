e70d24f8ee52a2a742014b4c9a925f87fa9ffa6f
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2021-08-09 11:53:48 -0700

    Add questions function

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,9 @@ def index():
     """Display the index page."""
     return render_template("index.html")
 
-@app.route("/about")
-def about():
-    return "This is an about page."
+@app.route("/questions", methods=["GET", "POST"])
+def questions():
+    return render_template("questions.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
